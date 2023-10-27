49ca6a80d16fd21a49d195b128ad4587da843ff8
Jim Hawkins <hawkinsji@vmware.com>
2023-10-27 13:30:27 +0100

    Add code and documentation for a certificate authority for testing purposes
    Signed-off-by: Jim Hawkins <hawkinsji@vmware.com>

---
+++ ca-openssl-cli/backlog.md
@@ -0,0 +1,60 @@
+# Backlog
+Add support for setting a longer expiry time.
+
+Maybe add testing of the certificates, for S/MIME for example see
+https://www.misterpki.com/openssl-smime/
+
+Review use of `critical` in key usage and extended key usage. Maybe it means
+that a certificate can't be used for anything that isn't allowed "that which
+isn't forbidden is allowed".  
+-   See https://security.stackexchange.com/questions/17583/how-do-i-create-a-valid-email-certificate-for-outlook-s-mime-with-openssl#17601
+-   https://security.stackexchange.com/a/264648
+
+Add support for multiple rfc822 or multiple UPN or other multiples in the SAN or
+wherever supported.
+
+Add the required field and value for this.
+
+-   https://support.microsoft.com/en-gb/topic/kb5014754-certificate-based-authentication-changes-on-windows-domain-controllers-ad2c23b0-15d8-4340-a468-4d4f3b188f16?utm_campaign=24Q2_IAM_Global_Customer-Notice_MSFT-CertAuth_Email&utm_medium=email&utm_source=Eloqua&edc_sfid=7015Y0000048249QAA#bkmk_compatmode
+
+>   From Entrust  
+>   The Full Enforcement mode requires, for newly issued certificates, checks of
+>   the Object Identifier (OID) (1.3.6.1.4.1.3.11.26.2) extension against their
+>   corresponding user account SID (Security Identifier) in AD/DC
+
+Add an option to add the key to a pkcs7 aka p7b file and export to pfx. That
+could be used to integrate with ADFS or other external CAs. Maybe the script
+should read the CN from the pkcs7 file in order to determine the name of the
+.key file. Handy command here.
+
+    openssl pkcs7 -print_certs -in example.com/user01.p7b
+
+Add a friendly name to client certificates, and CA certificates.  
+See the `openssl` `pkcs12` CLI `-name` switch. Or maybe the `-setalias` switch,
+see https://serverfault.com/a/103314.
+
+Add an option to list the expiry dates of all the certificates in the store.
+Here's a CLI to do that.
+
+    openssl x509 -in example.com/user01_Auth.cer -noout -dates 
+
+And see
+https://www.ssl.com/how-to/export-certificates-private-key-from-pkcs12-file-with-openssl/
+
+    openssl pkcs12 -info -in depot/user01.p12 -nodes
+
+Add an option to load the certs onto a YubiKey using the ykman CLI.
+https://docs.yubico.com/software/yubikey/tools/ykman/PIV_Commands.html
+
+Add an option for EC keys instead of RSA keys.
+
+Add more prominent error messages for when openssl returns an error.
+
+See about using the ?new `openssl` `ca` command for signing and maybe CA
+creation.
+
+# License
+Copyright 2023 VMware, Inc. All rights reserved.  
+The Workspace ONE PIV-D Manager integration samples are licensed under a
+two-clause BSD license.  
+SPDX-License-Identifier: BSD-2-Clause
---
+++ ca-openssl-cli/certauth/certificate_configuration.py
@@ -0,0 +1,192 @@
+# Copyright 2023 VMware, Inc.
+# SPDX-License-Identifier: BSD-2-Clause
+
+# Run with Python 3.7 or later.
+"""File in the certauth module."""
+#
+# Uses the following recent Python features.
+# -   Python 3.7 subprocess text output and capture_output.
+#
+# Standard library imports, in alphabetic order.
+#
+# Module for OO path handling.
+# https://docs.python.org/3/library/pathlib.html
+from pathlib import Path
+
+from certauth.certificate_purpose import CertificatePurpose
+
+class CertificateConfiguration:
+
+    # Properties for cartauth CLI.
+    @property
+    def countryCode(self):
+        return self._countryCode
+    @countryCode.setter
+    def countryCode(self, countryCode):
+        self._countryCode = countryCode
+
+    @property
+    def stateName(self):
+        return self._stateName
+    @stateName.setter
+    def stateName(self, stateName):
+        self._stateName = stateName
+
+    @property
+    def localityName(self):
+        return self._localityName
+    @localityName.setter
+    def localityName(self, localityName):
+        self._localityName = localityName
+
+    @property
+    def organisationName(self):
+        return self._organisationName
+    @organisationName.setter
+    def organisationName(self, organisationName):
+        self._organisationName = organisationName
+
+    @property
+    def organisationalUnitName(self):
+        return self._organisationalUnitName
+    @organisationalUnitName.setter
+    def organisationalUnitName(self, organisationalUnitName):
+        self._organisationalUnitName = organisationalUnitName
+
+    @property
+    def purposesSpecifier(self):
+        return self._purposesSpecifier
+    @purposesSpecifier.setter
+    def purposesSpecifier(self, purposesSpecifier):
+        self._purposesSpecifier = purposesSpecifier
+
+    # End of CLI properties.
+
+    # Computed property.
+    @property
+    def certificatesPurposes(self):
+        return self._certificatesPurposes
+
+    # End of computer property.
+
+    def _write_one_CNF(self, cnfPath, email, purposes):
+        # TOTH
+        #
+        # CNF file:
+        # https://www.golinuxcloud.com/openssl-generate-csr-create-san-certificate/
+        # https://www.golinuxcloud.com/openssl-create-client-server-certificate/
+        #
+        # How to put UPN into a certificate:
+        # https://mchesnavsky.tech/generate-ssl-certificate-with-user-principal-name-openssl/
+        #
+        # Long identifiers for fields in the distinguished name:  
+        # https://www.openssl.org/docs/man3.0/man1/openssl-req.html  
+        # Look for stateOrProvinceName.
+        #
+        # Subject Alternate Name in a CNF file for a CSR.
+        # https://www.ibm.com/support/pages/how-create-csr-multiple-subject-alternative-name-san-entries-pase-openssl-3rd-party-or-internet-ca
+        #
+        # Some other useful looking pages:
+        # https://sockettools.com/kb/creating-certificate-using-openssl/
+        # https://www.openssl.org/docs/manmaster/man5/x509v3_config.html
+
+        emailCNF = email.replace('#', '\#')
+
+        keyUsages = []
+        extendedKeyUsages = []
+        for purpose in purposes:
+            for usage in purpose.keyUsages:
+                if usage not in keyUsages:
+                    keyUsages.append(usage)
+            for usage in purpose.extendedKeyUsages:
+                if usage not in extendedKeyUsages:
+                    extendedKeyUsages.append(usage)
+        
+        keyUsagesCNF = (
+            "" if len(keyUsages) == 0 else
+            ''.join(( 'keyUsage = ', (", ".join(keyUsages)), "\n" ))
+        )
+        extendedKeyUsagesCNF = (
+            "" if len(extendedKeyUsages) == 0 else
+            ''.join((
+                'extendedKeyUsage = ', (", ".join(extendedKeyUsages)), "\n"
+            ))
+        )
+
+        if email != emailCNF:
+            print(f'CNF email "{emailCNF}"')
+        cnfPath.write_text(f'''# Written by certauth module.
+basicConstraints = CA:FALSE
+subjectKeyIdentifier = hash
+authorityKeyIdentifier = keyid,issuer
+subjectAltName = @alt_names
+{keyUsagesCNF}{extendedKeyUsagesCNF}
+[alt_names]
+otherName = 1.3.6.1.4.1.311.20.2.3;UTF8:{emailCNF}
+email = {emailCNF}
+
+[req]
+prompt = no
+distinguished_name = distinguished_names
+req_extensions = req_extensions
+
+[req_extensions]
+subjectAltName = @alt_names
+{keyUsagesCNF}{extendedKeyUsagesCNF}
+[distinguished_names]
+commonName = {emailCNF}
+countryName = {self.countryCode}
+stateOrProvinceName = {self.stateName}
+localityName = {self.localityName}
+emailAddress = {emailCNF}
+organizationName = {self.organisationName}
+organizationalUnitName = {self.organisationalUnitName}
+'''
+# deprecated.
+# nsCertType = client, email
+# nsComment = "OpenSSL Generated Client Certificate"
+
+        )
+
+        return cnfPath
+
+    def write_client_CNFs(self, depotPath, clientName, email):
+        # On the next line
+        #
+        # -   humanSuffixes() is the default of all purposes.
+        # -   [1] gets the certificate long forms.
+        # -   [0] gets the first long form and there will be only one
+        #     certificate.
+        defaultSuffix = CertificatePurpose.suffix(
+            CertificatePurpose.parsePurposesSpecifier(
+                CertificatePurpose.humanSuffixes())[1][0])
+
+        for purposes in self.certificatesPurposes:
+            suffix = CertificatePurpose.suffix(purposes)
+            if suffix == defaultSuffix:
+                suffix = ""
+            # Add a dummy suffix in case the client name includes a dot. The
+            # with_suffix() method replaces everything after the last dot in the
+            # stem.
+            cnfPath = Path(
+                depotPath, "".join((clientName, suffix, ".dummySuffix"))
+            ).resolve().with_suffix(".cnf")
+            yield self._write_one_CNF(cnfPath, email, purposes)
+
+    def parsePurposesSpecifier(self):
+        shortForm, certificates, ok, reports = (
+            CertificatePurpose.parsePurposesSpecifier(self.purposesSpecifier))
+        if not(
+            ok and self.purposesSpecifier == CertificatePurpose.humanSuffixes()
+        ):
+            print(f'Purposes "{self.purposesSpecifier}".')
+            if shortForm != self.purposesSpecifier:
+                print(f'Parsed short form "{shortForm}".')
+            print(f"Certificates {len(certificates)}:")
+            for index, report in enumerate(reports):
+                print(index + 1, report)
+
+        self._certificatesPurposes = certificates[:]
+
+        return ok
+
