1726ed1049eb8747c8bc26ba520a616b5fe5a372
Krishna Shanbhag <krishanbhag@gmail.com>
2024-10-24 16:51:16 +0530

    copyright-update
    copyright-update

--- iOSPIVDSamples/SamplePivdTokenApp/SamplePivdTokenApp/Views/WebAuthViewController.swift
+++ iOSPIVDSamples/SamplePivdTokenApp/SamplePivdTokenApp/Views/WebAuthViewController.swift
@@ -1,6 +1,12 @@
 //
 //  WebAuthViewController.swift
 //  SamplePivdTokenApp
+//
+//  Copyright (c) 2025 Omnissa, LLC. All rights reserved.
+//  This product is protected by copyright and intellectual property laws in the
+//  United States and other countries as well as by international treaties.
+//  -- Omnissa Public
+//
 
 import UIKit
 import WebKit
